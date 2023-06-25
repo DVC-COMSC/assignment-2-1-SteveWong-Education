e79e716cb0f0d541921a3d6385130449e0930b2c
SteveWong-Education <136470980+SteveWong-Education@users.noreply.github.com>
2023-06-24 22:43:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 def main():
-    
 
-    
     m_perc = input()
     f_perc = input()
 
     total = float(m_perc) + float(f_perc)
-    
+
     print("The total number of students:   \t" + format(total, '.0f' ))
     print(f"The number of males and females \t{m_perc}      \t{f_perc}")
-    m_perc = float(m_perc)/100
-    f_perc = float(f_perc)/100
+    temp1 = m_perc
+    temp2 = f_perc
+    m_perc = float(m_perc)/total 
+    f_perc = float(f_perc)/total 
     print(f"The percentage of males and females     {format((m_perc), '.2%')}          {format((f_perc), '.2%')}")
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
+    m_perc = temp1
+    f_perc = temp2
     return m_perc, f_perc
 
 
