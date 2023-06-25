2bcb2cda1c6268ec04a61cc0fdcaacc8a0a6994d
SteveWong2008 <77519023+SteveWong2008@users.noreply.github.com>
2023-06-24 22:18:06 -0700

    Assignment 1

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    m_perc = input()
+    f_perc = input()
 
+    total = float(m_perc) + float(f_perc)
+    
+    print("The total number of students:   \t" + format(total, '.0f' ))
+    print(f"The number of males and females \t{m_perc}      \t{f_perc}")
+    m_perc = float(m_perc)/100
+    f_perc = float(f_perc)/100
+    print(f"The percentage of males and females     {format((m_perc), '.2%')}          {format((f_perc), '.2%')}")
     """
     ########################################
     # Do not delete the return statement
