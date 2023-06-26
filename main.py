4b9e36871031b74bac6cc32fa191cd82cb4cda09
SteveWong-Education <136470980+SteveWong-Education@users.noreply.github.com>
2023-06-26 16:02:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
 
     print("The total number of students:   \t" + format(total, '.0f' ))
     print(f"The number of males and females \t{m_perc}      \t{f_perc}")
-    temp1 = m_perc
-    temp2 = f_perc
-    m_perc = float(m_perc)/total 
-    f_perc = float(f_perc)/total 
-    print(f"The percentage of males and females     {format((m_perc), '.2%')}          {format((f_perc), '.2%')}")
-    m_perc = temp1
-    f_perc = temp2
+    
+    temp1 = float(m_perc)/total 
+    temp2 = float(f_perc)/total 
+    print(f"The percentage of males and females     {format((temp1), '.2%')}          {format((temp2), '.2%')}")
+
+    m_perc = float(temp1)*100
+    f_perc = float(temp2)*100
     return m_perc, f_perc
 
 
